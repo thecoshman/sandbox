aeabb27a82c645bf12b03bf641f8be9b6bf3cc66
thecoshman <thecoshman@gmail.com>
2013-06-19 07:53:52 +0100

    Tweaked 'slurping' code Should now reduce number of copies

--- src/main.cpp
+++ src/main.cpp
@@ -32,9 +32,16 @@ std::string loadShader(const std::string& sourceFile){
         std::cerr << "Failed top open '" << sourceFile << "'" << std::endl;
         return "";
     }
-    std::stringstream stream;
-    stream << file.rdbuf();
-    return stream.str();
+    //std::stringstream stream;
+    //stream << file.rdbuf();
+
+    std::string slurped;
+    file.seekg(0, std::ios::end);
+    slurped.resize(file.tellg());
+    file.seekg(0, std::ios::beg);
+    file.read(&slurped[0], slurped.length());
+
+    return slurped;
 }
 
 namespace Peanuts{
